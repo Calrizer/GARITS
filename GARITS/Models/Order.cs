cb59f7bdb89b55172581d2c9708f17e34bac975d
zalrox <james.avery@city.ac.uk>
2019-04-03 18:33:29 +0100

    Customer purchase of parts implemented

---
+++ GARITS/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GARITS.Models
+{
+    public class Order
+    {
+        public string orderID { get; set; }
+        public DateTime date { get; set; }
+        public DateTime deliveredDate { get; set; }
+        public string addressline1 { get; set; }
+        public string addressline2 { get; set; }
+        public string town { get; set; }
+        public string county { get; set; }
+        public string postcode { get; set; }
+        public string username { get; set; }
+    }
+}
--- GARITS/Providers/PartsProvider.cs
+++ GARITS/Providers/PartsProvider.cs
@@ -176,7 +176,35 @@ namespace GARITS.Providers
 
         }
 
-                public static void addNewPart(Part part)
+        public static void partsOrder(Order order)
+        {
+            using (MySqlConnection con = new MySqlConnection(connection))
+            {
+                string query = "INSERT INTO Orders (orderId, date, addressLine1, addressLine2, town, county, postcode, username)" +
+                    " VALUES (@orderID, @date, @addressLine1, @addressLine2, @town, @county, @postcode, @username)";
+                using (MySqlCommand cmd = new MySqlCommand(query))
+                {
+                    cmd.Parameters.AddWithValue("@orderID", order.orderID);
+                    cmd.Parameters.AddWithValue("@date", order.date);
+                    cmd.Parameters.AddWithValue("@addressLine1", order.addressline1);
+                    cmd.Parameters.AddWithValue("@addressLine2", order.addressline2);
+                    cmd.Parameters.AddWithValue("@town", order.town);
+                    cmd.Parameters.AddWithValue("@county", order.county);
+                    cmd.Parameters.AddWithValue("@postcode", order.postcode);
+                    cmd.Parameters.AddWithValue("@username", order.username);
+
+                    cmd.Connection = con;
+                    con.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    con.Close();
+                }
+            }
+        }
+
+
+        public static void addNewPart(Part part)
         {
             using (MySqlConnection con = new MySqlConnection(connection))
             {
@@ -287,6 +315,136 @@ namespace GARITS.Providers
                     con.Close();
 
                     return parts;
+                }
+            }
+        }
+
+        public static void AddPartToOrder(string orderID, string partID, int quantity)
+        {
+            using (MySqlConnection con = new MySqlConnection(connection))
+            {
+                string query = "INSERT INTO PartOrder VALUES (@orderID, @partID, @quantity)";
+                using (MySqlCommand cmd = new MySqlCommand(query))
+                {
+                    cmd.Parameters.AddWithValue("@orderID", orderID);
+                    cmd.Parameters.AddWithValue("@partID", partID);
+                    cmd.Parameters.AddWithValue("@quantity", quantity);
+
+                    cmd.Connection = con;
+                    con.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    con.Close();
+
+                }
+            }
+        }
+        public static Dictionary<Part, int> getOrder(string orderID)
+        {
+            Dictionary<Part, int> parts = new Dictionary<Part, int>();
+
+            using (MySqlConnection con = new MySqlConnection(connection))
+            {
+                string query = "SELECT * FROM PartOrder WHERE orderID = @orderID";
+                using (MySqlCommand cmd = new MySqlCommand(query))
+                {
+                    cmd.Parameters.AddWithValue("@orderID", orderID);
+                    cmd.Connection = con;
+                    con.Open();
+
+                    using (MySqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+
+                            parts.Add(PartsProvider.getPartFromID(sdr["partID"].ToString()), int.Parse(sdr["quantity"].ToString()));
+                        }
+                    }
+                    con.Close();
+                    
+                }
+            }
+            return parts;
+        }
+
+        public static Order getOrderDetails(string orderID)
+        {
+            Order order = new Order();
+            using (MySqlConnection con = new MySqlConnection(connection))
+            {
+                string query = "SELECT * FROM Orders WHERE orderID = @orderID";
+                using (MySqlCommand cmd = new MySqlCommand(query))
+                {
+                    cmd.Parameters.AddWithValue("@orderID", orderID);
+                    cmd.Connection = con;
+                    con.Open();
+
+                    using (MySqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+
+                            order = new Order
+                            {
+                                orderID = sdr["orderID"].ToString(),
+                                date = DateTime.Parse(sdr["date"].ToString()),
+                                addressline1 = sdr["addressLine1"].ToString(),
+                                addressline2 = sdr["addressLine2"].ToString(),
+                                town = sdr["town"].ToString(),
+                                county = sdr["county"].ToString(),
+                                postcode = sdr["postcode"].ToString(),
+                                username = sdr["username"].ToString()
+                            };
+                        }
+                    }
+                    con.Close();
+
+                }
+            }
+            return order;
+
+        }
+
+
+        public static void PartOrderAddQuantity(string orderID, string partID, int quantity)
+        {
+            using (MySqlConnection con = new MySqlConnection(connection))
+            {
+                string query = "UPDATE PartOrder set quantity = (quantity + @quantity) WHERE partID = @partID AND orderID = @orderID";
+                using (MySqlCommand cmd = new MySqlCommand(query))
+                {
+                    cmd.Parameters.AddWithValue("@orderID", orderID);
+                    cmd.Parameters.AddWithValue("@partID", partID);
+                    cmd.Parameters.AddWithValue("@quantity", quantity);
+
+                    cmd.Connection = con;
+                    con.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    con.Close();
+
+                }
+            }
+        }
+
+        public static void clearOrder(string orderID)
+        {
+            using (MySqlConnection con = new MySqlConnection(connection))
+            {
+                string query = "DELETE FROM PartOrder WHERE orderID = @orderID";
+                using (MySqlCommand cmd = new MySqlCommand(query))
+                {
+                    cmd.Parameters.AddWithValue("@orderID", orderID);
+
+                    cmd.Connection = con;
+                    con.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    con.Close();
+
                 }
             }
         }
